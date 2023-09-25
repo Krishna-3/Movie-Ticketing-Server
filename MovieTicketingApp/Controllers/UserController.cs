c99563686adfb49beb79a7a542aec7f07d34e5c9
Krishna-3 <pulipati.krishnavamsi@gmail.com>
2023-09-25 21:29:24 +0530

    created user controller

---
+++ MovieTicketingApp/Controllers/UserController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MovieTicketingApp.Interfaces;
+using MovieTicketingApp.Models;
+
+namespace MovieTicketingApp.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class UserController : Controller
+    {
+        private readonly IUserRepository _userRepository;
+
+        public UserController(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;   
+        }
+
+        [HttpPost]
+        [ProducesResponseType(201)]
+        [ProducesResponseType(400)]
+        public IActionResult CreateUser([FromBody] User user)
+        {
+            if(user == null) 
+                return BadRequest();
+
+            if(!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            if(!_userRepository.CreateUser(user))
+            {
+                ModelState.AddModelError("message", "user could not be saved");
+                return StatusCode(500, ModelState);
+            }
+
+            return Ok("Successfully user created");
+        }
+    }
+}
