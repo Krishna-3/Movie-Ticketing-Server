c53f8bc3dd4ee480f029bd0078fe4871a7933b3f
Krishna-3 <pulipati.krishnavamsi@gmail.com>
2023-09-27 17:59:19 +0530

    mapper updated

---
+++ MovieTicketingApp/Helper/MappingProfiles.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using MovieTicketingApp.DTO;
+using MovieTicketingApp.Models;
+
+namespace MovieTicketingApp.Helper
+{
+    public class MappingProfiles:Profile
+    {
+        public MappingProfiles()
+        {
+            CreateMap<User, UserDto>();
+            CreateMap<Movie, MovieEnDto>();
+            CreateMap<Movie, MovieHiDto>();
+            CreateMap<Movie, MovieTeDto>();
+            CreateMap<Theatre, TheatreEnDto>();
+            CreateMap<Theatre, TheatreHiDto>();
+            CreateMap<Theatre, TheatreTeDto>();
+            CreateMap<Ticket, TicketDto>();
+        }
+    }
+}
