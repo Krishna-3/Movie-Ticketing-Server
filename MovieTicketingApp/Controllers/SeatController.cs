953c3c81c5ec4fccea93bf943afba645fb517cfc
Krishna-3 <pulipati.krishnavamsi@gmail.com>
2023-09-27 21:12:08 +0530

    created seat controller

---
+++ MovieTicketingApp/Controllers/SeatController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MovieTicketingApp.Interfaces;
+using MovieTicketingApp.Models;
+
+namespace MovieTicketingApp.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class SeatController : ControllerBase
+    {
+        private readonly ISeatRepository _seatRepository;
+
+        public SeatController(ISeatRepository seatRepository)
+        {
+            _seatRepository = seatRepository;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(200, Type = typeof(IEnumerable<Seat>))]
+        [ProducesResponseType(400)]
+        public IActionResult GetSeats()
+        {
+            var seats = _seatRepository.GetSeats();
+
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            return Ok(seats);
+        }
+    }
+}
