70c090ad65458ee3104e19bea665d99f2a7b0e37
Krishna-3 <pulipati.krishnavamsi@gmail.com>
2023-09-27 21:37:15 +0530

    added validaitons

--- MovieTicketingApp/Models/Location.cs
+++ MovieTicketingApp/Models/Location.cs
@@ -1,9 +1,12 @@
-﻿namespace MovieTicketingApp.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MovieTicketingApp.Models
 {
     public class Location
     {
         public int Id { get; set; }
 
+        [RegularExpression("^[a-zA-Z]{2,29}$")]
         public string City { get; set; }
     }
 }
