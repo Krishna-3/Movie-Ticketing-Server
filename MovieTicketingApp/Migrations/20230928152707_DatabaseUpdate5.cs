2202cc2597705a515ae903c8c4aabf2e4b5f2fbf
Krishna-3 <pulipati.krishnavamsi@gmail.com>
2023-09-28 20:59:07 +0530

    updated database added Movies.Language

---
+++ MovieTicketingApp/Migrations/20230928152707_DatabaseUpdate5.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MovieTicketingApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class DatabseUpdate5 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Language",
+                table: "Movies",
+                newName: "LanguageTe");
+
+            migrationBuilder.AddColumn<string>(
+                name: "LanguageEn",
+                table: "Movies",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "LanguageHi",
+                table: "Movies",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "LanguageEn",
+                table: "Movies");
+
+            migrationBuilder.DropColumn(
+                name: "LanguageHi",
+                table: "Movies");
+
+            migrationBuilder.RenameColumn(
+                name: "LanguageTe",
+                table: "Movies",
+                newName: "Language");
+        }
+    }
+}
