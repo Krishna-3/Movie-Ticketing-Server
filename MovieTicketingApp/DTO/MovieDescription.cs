35e39e6e9537bfe9994ba6d22248f7be0ff32a42
Krishna-3 <pulipati.krishnavamsi@gmail.com>
2023-09-28 20:48:46 +0530

    created movie title and description dto's

---
+++ MovieTicketingApp/DTO/MovieDescription.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MovieTicketingApp.DTO
+{
+    public class MovieDescription
+    {
+        [RegularExpression("^[a-zA-Z 0-9]{2,}$")]
+        public string DescriptionEn{ get; set; }
+
+        [RegularExpression("^[a-zA-Z 0-9]{2,}$")]
+        public string DescriptionHi{ get; set; }
+
+        [RegularExpression("^[a-zA-Z 0-9]{2,}$")]
+        public string DescriptionTe{ get; set; }
+    }
+}
---
+++ MovieTicketingApp/DTO/MovieTitle.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MovieTicketingApp.DTO
+{
+    public class MovieTitle
+    {
+        [RegularExpression("^[a-zA-Z 0-9]{2,29}$")]
+        public string TitleEn { get; set; }
+
+        [RegularExpression("^[a-zA-Z 0-9]{2,29}$")]
+        public string TitleHi { get; set; }
+
+        [RegularExpression("^[a-zA-Z 0-9]{2,29}$")]
+        public string TitleTe { get; set; }
+    }
+}
