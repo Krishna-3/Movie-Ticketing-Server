042cd80da27c80b5bf88a9aa795e734066875861
Krishna-3 <pulipati.krishnavamsi@gmail.com>
2023-09-28 14:19:20 +0530

    updated state

--- MovieTicketingApp/Models/State.cs
+++ MovieTicketingApp/Models/State.cs
@@ -2,11 +2,14 @@
 
 namespace MovieTicketingApp.Models
 {
-    public class Language
+    public class State
     {
         public List<string> LanguageCode = new(){ "en", "hi", "te"};
 
         [RegularExpression("^[a-zA-Z]{2,2}$")]
         public string preferredLanguage{ get; set; }
+
+        [RegularExpression("^[a-zA-Z]{2,29}$")]
+        public string selectedLocation {  get; set; }   
     }
 }
