f0ce80b1bcf11675527c96a8fa67c3441af5ae04
Krishna-3 <pulipati.krishnavamsi@gmail.com>
2023-09-28 13:57:44 +0530

    added validations

--- MovieTicketingApp/Models/Language.cs
+++ MovieTicketingApp/Models/Language.cs
@@ -1,7 +1,12 @@
-﻿namespace MovieTicketingApp.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MovieTicketingApp.Models
 {
     public class Language
     {
+        public List<string> LanguageCode = new(){ "en", "hi", "te"};
+
+        [RegularExpression("^[a-zA-Z]{2,2}$")]
         public string preferredLanguage{ get; set; }
     }
 }
