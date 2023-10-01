cf843e05b7bee72dd10f3c21698ccf16c3153ad8
Krishna-3 <pulipati.krishnavamsi@gmail.com>
2023-10-01 22:05:10 +0530

    created token generators

---
+++ MovieTicketingApp/Services/TokenGenerators/AccessTokenGenerator.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using MovieTicketingApp.Authentication.Models;
+using MovieTicketingApp.Models;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace MovieTicketingApp.Services.TokenGenerators
+{
+    public class AccessTokenGenerator
+    {
+        private readonly AuthenticationConfiguraiton _configuration;
+
+        public AccessTokenGenerator(AuthenticationConfiguraiton configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public string GenerateToken(User user)
+        {
+            SecurityKey key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration.AccessTokenSecret));
+            SigningCredentials credentials = new(key, SecurityAlgorithms.HmacSha256);
+
+            List<Claim> claims = new()
+            {
+                new Claim("Id", user.Id.ToString()),
+                new Claim(ClaimTypes.Email, user.Email),
+                new Claim(ClaimTypes.Name, user.Username),
+                new Claim(ClaimTypes.Role, user.Role)
+            };
+
+            JwtSecurityToken token = new(
+                _configuration.Issuer,
+                _configuration.Audience,
+                claims,
+                DateTime.UtcNow,
+                DateTime.UtcNow.AddMinutes(_configuration.AccessTokenExpirationMinutes),
+                credentials);
+
+            return new JwtSecurityTokenHandler().WriteToken(token);  
+        }
+    }
+}
---
+++ MovieTicketingApp/Services/TokenGenerators/RefreshTokenGenerator.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using MovieTicketingApp.Authentication.Models;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
+
+namespace MovieTicketingApp.Services.TokenGenerators
+{
+    public class RefreshTokenGenerator
+    {
+        private readonly AuthenticationConfiguraiton _configuration;
+
+        public RefreshTokenGenerator(AuthenticationConfiguraiton configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public string GenerateToken()
+        {
+            SecurityKey key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration.RefreshTokenSecret));
+            SigningCredentials credentials = new(key, SecurityAlgorithms.HmacSha256);
+
+            JwtSecurityToken token = new(
+                _configuration.Issuer,
+                _configuration.Audience,
+                null,
+                DateTime.UtcNow,
+                DateTime.UtcNow.AddMinutes(_configuration.RefreshTokenExpirationDays),
+                credentials);
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
