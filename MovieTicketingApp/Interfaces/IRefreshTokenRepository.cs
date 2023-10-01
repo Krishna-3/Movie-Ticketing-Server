4412ec68569382925f979bd9f249150f5c3366e5
Krishna-3 <pulipati.krishnavamsi@gmail.com>
2023-10-01 22:07:03 +0530

    created refresh token repository

---
+++ MovieTicketingApp/Interfaces/IRefreshTokenRepository.cs
@@ -0,0 +1,21 @@
+﻿using MovieTicketingApp.Models;
+
+namespace MovieTicketingApp.Interfaces
+{
+    public interface IRefreshTokenRepository
+    {
+        bool RefreshTokenExists(string token);
+
+        RefreshToken GetRefreshToken(string token);
+
+        IEnumerable<RefreshToken> GetAllRefreshTokens(int UserId);
+
+        bool AddRefreshToken(RefreshToken token);
+
+        bool UpdateRefreshToken(RefreshToken token);
+
+        bool DeleteAllRefreshToken(IEnumerable<RefreshToken> tokens);
+
+        bool Save();
+    }
+}
---
+++ MovieTicketingApp/Repository/RefreshTokenRepository.cs
@@ -0,0 +1,68 @@
+﻿using MovieTicketingApp.Data;
+using MovieTicketingApp.Interfaces;
+using MovieTicketingApp.Models;
+
+namespace MovieTicketingApp.Repository
+{
+    public class RefreshTokenRepository : IRefreshTokenRepository
+    {
+        private readonly DataContext _context;
+
+        public RefreshTokenRepository(DataContext context)
+        {
+            _context = context;
+        }
+
+        public bool UpdateRefreshToken(RefreshToken token)
+        {
+            _context.RefreshTokens.Update(token);
+
+            return Save();
+        }
+
+        public RefreshToken GetRefreshToken(string token)
+        {
+            return _context.RefreshTokens.First(t => t.Token == token);
+        }
+
+        public bool RefreshTokenExists(string token)
+        {
+            var tokenExists = _context.RefreshTokens.FirstOrDefault(r => r.Token == token);
+
+            if (tokenExists == null)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        public bool AddRefreshToken(RefreshToken token)
+        {
+            _context.RefreshTokens.Add(token);
+
+            return Save();
+        }
+
+        public bool Save()
+        {
+            var saved = _context.SaveChanges();
+
+            return saved > 0;
+        }
+
+        public bool DeleteAllRefreshToken(IEnumerable<RefreshToken> tokens)
+        {
+            _context.RefreshTokens.RemoveRange(tokens);
+
+            return Save();
+        }
+
+        public IEnumerable<RefreshToken> GetAllRefreshTokens(int UserId)
+        {
+            var refreshTokens = _context.RefreshTokens.Where(t => t.UserId == UserId);
+
+            return refreshTokens;
+        }
+    }
+}
