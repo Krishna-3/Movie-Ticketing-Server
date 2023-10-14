ce6aa931437bf79dfea7873d783271f333e786b8
Krishna-3 <pulipati.krishnavamsi@gmail.com>
2023-10-14 15:16:21 +0530

    created seatdto

---
+++ MovieTicketingApp/DTO/SeatDto.cs
@@ -0,0 +1,12 @@
+﻿
+namespace MovieTicketingApp.DTO
+{
+    public class SeatDto
+    {
+        public int Id { get; set; }
+
+        public string SeatNumber { get; set; }
+
+        public bool Booked {get;set; } 
+    }
+}
